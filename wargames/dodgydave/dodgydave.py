b20291b4e80cd038008efd1dfacec2eb6c8eb7d2
Gareth McFarlane <gmcf8558@uni.sydney.edu.au>
2016-03-13 12:56:54 +1100

    Dodgy dave test

--- wargames/dodgydave/dodgydave.py
+++ wargames/dodgydave/dodgydave.py
@@ -2,13 +2,30 @@ import random
 
 from Crypto.Hash import SHA256
 
+dict = {}
+
+msga = "Melbourne Cup: $5,000 on Horse A (Bondi Beach) [transaction id: {%d]"
+
+for ID in range(0,999999):
+    ma_dash = msga % ID
+    ma_dash_hash = SHA256.new(ma_dash.encode("ascii")).hexdigest()[:32]
+    dict[ma_dash_hash] = ma_dash
 
 
-msga = "Melbourne Cup: $5,000 on Horse A (Bondi Beach) [transaction id: {10]"
-msgb = "Melbourne Cup: $5,000 on Horse B (Who Shot Thebarman) [transaction id: {10}]"
+    
 
 
-b_hash = SHA256.new(msgb.encode("ascii")).hexdigest()[:16]
+msgb = "Melbourne Cup: $5,000 on Horse B (Who Shot Thebarman) [transaction id: {%d}]"
 
 
-print(b_hash)
+for ID in range(0,999999):
+    mb_dash = msgb % ID
+    mb_dash_hash = SHA256.new(mb_dash.encode("ascii")).hexdigest()[:32]
+    if mb_dash_hash in dict:
+        print(mb_dash_hash)
+        print(dict[mb_dash_hash])
+
+
+
+
+
