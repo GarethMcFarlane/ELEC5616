cd1cb88194df63b8d871042e9d4bc7db3ded8a01
Gareth McFarlane <gmcf8558@uni.sydney.edu.au>
2016-03-13 14:40:04 +1100

    Tidy up code and comments

--- wargames/mallory.py
+++ wargames/mallory.py
@@ -4,22 +4,17 @@ import random
 
 from Crypto.Hash import SHA256
 
-
+# Get initial message and hash.
 m = "Your friend Mallory changed her phone number to 027625"
 m_hash = SHA256.new(m.encode("ascii")).hexdigest()[:7]
 
 
 
-
-
-
-
-# Try to find hash collisions for increasingly lengthier hashes
+# We only want the first 7 hex characters of the hash.
 shortened_to = 7
-dict = {}
-
 
 while True:
+  # Generate random string and hash it.
   randomStr = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
   h_attempt = SHA256.new(randomStr.encode("ascii")).hexdigest()[:shortened_to]
   if (h_attempt == m_hash):
