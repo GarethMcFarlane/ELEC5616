30881da7d987ee9b1954d56f1527aeadf8587a78
GarethMcFarlane <gmcf8558@uni.sydney.edu.au>
2016-03-18 13:35:29 +1100

    Added md5 brute force attack

---
+++ wargames/md5.py
@@ -0,0 +1,19 @@
+from time import time
+import string
+import random
+
+from Crypto.Hash import MD5
+
+hash = "f85ea6afca086f6cda7d9ceb05545cab"
+
+
+while True:
+  # Generate random string and hash it.
+  randomStr = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+  h_attempt = MD5.new(randomStr.encode("ascii")).hexdigest()
+  print(h_attempt)
+  if (h_attempt == hash):
+    print(randomStr)
+    break
+
+
